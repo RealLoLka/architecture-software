f1819d277995fc4063fb860f3f14ae1932a28883
RealLoLka <4isteg@gmail.com>
2023-10-31 19:57:00 +0300

    .

--- database/basket.h
+++ database/basket.h
@@ -12,7 +12,7 @@ namespace database
         private:
             long _id;
             long _user_id;
-            long _service_id;
+            long _product_id;
             //time_t _creation_date;
 
         public:
@@ -21,11 +21,11 @@ namespace database
 
             long             get_id() const;
             long             get_user_id() const;
-            long             get_service_id() const;
+            long             get_product_id() const;
 
             long&        id();
             long&        user_id();
-            long&        service_id();
+            long&        product_id();
 
             static void init();
             static std::vector<Basket> read_by_user_id(long user_id);
